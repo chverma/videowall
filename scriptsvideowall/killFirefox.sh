f2db29e798ade84383999c54be515aafecf29d1c
chverma <chverma@users.noreply.github.com>
2025-04-08 12:37:42 +0200

    Add show firefox and kill

---
+++ scriptsvideowall/killFirefox.sh
@@ -0,0 +1,9 @@
+declare -a monitors=( 11 12 13 14 21 22 23 24 31 32 33 34 )
+declare -a rows=( 0 0 0 0 1 1 1 1 2 2 2 2 )
+declare -a columns=( 0 1 2 3 0 1 2 3 0 1 2 3 )
+
+for ((i=0;i<12;i++))
+do
+    ssh "admin@192.168.1.${monitors[$i]}" "killall firefox" &
+
+done
